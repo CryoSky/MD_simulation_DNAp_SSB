77435f71a0452f86fef52b8ed916d83b6c79cec4
Shikai Jin <568702765@qq.com>
2024-05-06 10:05:11 -0700

    Update calculate_q_value_pdb1_pdb2_var_length_multi_chain.py

--- result_analysis/calculate_q_value_pdb1_pdb2_var_length_multi_chain.py
+++ result_analysis/calculate_q_value_pdb1_pdb2_var_length_multi_chain.py
@@ -3,7 +3,7 @@
 
 # A greatly expansion of the calculate Q script, now support multiple chains
 # Remeber only works for ATOM, not for HETATM
-# Example in Linux: python /mnt/e/linux/script/Calculate_Q_value_pdb1_pdb2_var_length.py T1003/3D-JIGSAW_SL1_TS1.pdb T1003_reference_6hrh_A.pdb 0 --start1 30 --end1 465
+# Example in Linux: python calculate_q_value_pdb1_pdb2_var_length_multi_chain.py T1003/3D-JIGSAW_SL1_TS1.pdb T1003_reference_6hrh_A.pdb 0 --start1 30 --end1 465
 
 
 __AUTHOR__ = 'Shikai Jin'
